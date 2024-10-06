fc99d7737d874de0f7dc9ae01194767fc7237f1f
EddieKe <otienoaudrey1@gmail.com>
2024-10-06 04:31:09 +0300

    created a new file chrat_section

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nasa_space_app2/constants.dart';
 import 'package:nasa_space_app2/sections/about/about_section.dart';
+import 'package:nasa_space_app2/sections/chart/chart_section.dart';
 import 'package:nasa_space_app2/sections/contact/contact_section.dart';
 import 'package:nasa_space_app2/sections/feeedback/feedback_section.dart';
 import 'package:nasa_space_app2/sections/topSection/components/top_section.dart';
@@ -17,10 +18,11 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            TopSection(),
+            const TopSection(),
             const SizedBox(height: kDefaultPadding * 2),
             const AboutSection(),
             const ServiceSection(),
+                   ChartSection(),
         //    RecentWorkSection(),
             const FeedbackSection(),
             const SizedBox(height: kDefaultPadding),
